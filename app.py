a511988e8a6e5e3e8b0eb3ba0132bedb59b30c32
Basic2009 <mateusz.wojtczak123@gmail.com>
2026-08-25 16:16:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -253,7 +253,7 @@ if st.session_state.get("obliczono", False):
             elif podejscie == "min_barwa":
                 prob += norm_barwa * 1000 + uzyte_tanki * 10
             elif podejscie == "min_oba":
-                prob += norm_kwas * 800 + norm_barwa * 100 + uzyte_tanki * 10
+                prob += norm_kwas * 700 + norm_barwa * 300 + uzyte_tanki * 10
 
             prob.solve(PULP_CBC_CMD(msg=0))
             if LpStatus[prob.status] != "Optimal":
